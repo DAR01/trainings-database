86d8ac6807d16570dc5c58f06d2db34cac97ab68
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2017-03-09 00:22:35 -0300

    Altera tile server para OSM

--- js/map.js
+++ js/map.js
@@ -15,13 +15,12 @@ jQuery(document).ready(function() {
 	var popupTemplate = jQuery('#popup-template').html();
 
 	var map = L.map('mapa-feiras').setView(curitiba, 13);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{
 		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(map);
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		}
+	).addTo(map);
 
 	jQuery.ajax({
 		url: "data/feiras-organicas-curitiba.json",
